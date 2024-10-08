0a4d913a8ebac8b870f91f52350a96ff29d1c00a
danielschoder <daniel@schoder.uk>
2024-10-08 16:38:36 +0100

    F1-14

--- Formula1.Api/Endpoints/ConstructorsEndpoints.cs
+++ Formula1.Api/Endpoints/ConstructorsEndpoints.cs
@@ -1,4 +1,5 @@
 ﻿using Formula1.Application.Commands.ImportCommands;
+using Formula1.Application.Queries;
 using Formula1.Contracts.Requests;
 using MediatR;
 
@@ -8,12 +9,12 @@ public static class ConstructorsEndpoints
 {
     public static void MapConstructorsEndpoints(this IEndpointRouteBuilder app)
     {
-        //app.MapGet("/api/constructors", ListConstructorsAsync);
+        app.MapGet("/api/constructors", ListConstructorsAsync);
         //app.MapGet("/api/constructors/{id:guid}", GetConstructorAsync);
         app.MapPost("/api/constructors", ImportConstructorsAsync);
 
-        //static async Task<IResult> ListConstructorsAsync(IMediator mediator)
-        //    => Results.Ok(await mediator.Send(new GetConstructorsQuery()));
+        static async Task<IResult> ListConstructorsAsync(IMediator mediator)
+            => Results.Ok(await mediator.Send(new GetConstructorsQuery()));
 
         //static async Task<IResult> GetConstructorAsync(Guid id, IMediator mediator)
         //    => Results.Ok(await mediator.Send(new GetConstructorByIdQuery(id)));
