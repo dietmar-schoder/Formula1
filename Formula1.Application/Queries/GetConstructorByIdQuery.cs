bfcc189cb1c84593bd493ef16a1c51428e61982e
danielschoder <daniel@schoder.uk>
2024-10-08 18:22:23 +0100

    F1-15

---
+++ Formula1.Application/Queries/GetConstructorByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using Formula1.Contracts.Dtos;
+using MediatR;
+
+namespace Formula1.Application.Queries;
+
+public class GetConstructorByIdQuery(Guid id) : IRequest<ConstructorDto>
+{
+    public Guid Id { get; set; } = id;
+}
