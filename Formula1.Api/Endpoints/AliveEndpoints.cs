0f3aec32636dfe7a5532ab9837c8c52f2ce008df
danielschoder <daniel@schoder.uk>
2024-10-10 17:24:17 +0100

    Add Scalar UI

--- Formula1.Api/Endpoints/AliveEndpoints.cs
+++ Formula1.Api/Endpoints/AliveEndpoints.cs
@@ -7,10 +7,8 @@ public static class AliveEndpoints
 {
     public static void MapAliveEndpoints(this IEndpointRouteBuilder app)
     {
-        app.MapGet("/", GetVersionAsync);
-        app.MapGet("/api", GetVersionAsync)
-            .WithTags("Alive")
-            .WithName("GetVersion");
+        app.MapGet("/", GetVersionAsync).WithOpenApi();
+        app.MapGet("/api", GetVersionAsync).WithOpenApi();
 
         static async Task<IResult> GetVersionAsync(IMediator mediator)
             => Results.Ok(await mediator.Send(new GetVersionQuery()));
--- Formula1.Api/Program.cs
+++ Formula1.Api/Program.cs
@@ -1,15 +1,23 @@
 using Formula1.Api.Endpoints;
 using Formula1.Api.Extensions;
-using Formula1.Infrastructure.Persistence;
-using Microsoft.EntityFrameworkCore;
-using System.Text.Json.Serialization;
-using Microsoft.AspNetCore.Http.Json;
 using Formula1.Application.Interfaces.Persistence;
 using Formula1.Application.Queries;
+using Formula1.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Http.Json;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Scalar.AspNetCore;
+using System.Text.Json.Serialization;
 
 #region Services
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Formula 1 API Reference", Version = "1.0" });
+});
 
 builder.Services.Configure<JsonOptions>(options =>
 {
@@ -21,13 +29,24 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddScoped<IApplicationDbContext>(provider => provider.GetService<ApplicationDbContext>());
 
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(GetVersionQueryHandler).Assembly));
-
 builder.Services.AddInfrastructureServices();
 
 #endregion
 #region Build
 
 var app = builder.Build();
+
+app.UseSwagger(options =>
+{
+    options.RouteTemplate = "openapi/{documentName}.json";
+});
+app.MapScalarApiReference(options =>
+{
+    options
+        .WithTitle("Formula 1 API Reference")
+        .WithSidebar(false)
+        .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient);
+});
 
 app.MapAliveEndpoints();
 app.MapCircuitsEndpoints();
