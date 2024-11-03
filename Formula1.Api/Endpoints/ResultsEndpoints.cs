b25e6a094c023b716425b51217921ad9bcd54e84
dietmar-schoder <dietmar@schoder.uk>
2024-11-03 19:23:03 +0000

    F1-45

--- Formula1.Api/Endpoints/ResultsEndpoints.cs
+++ Formula1.Api/Endpoints/ResultsEndpoints.cs
@@ -1,4 +1,5 @@
-﻿using Formula1.Application.Interfaces.Services;
+﻿using Formula1.Application.Handlers.QueryHandlers;
+using Formula1.Application.Interfaces.Services;
 using Formula1.Application.Queries;
 using MediatR;
 
@@ -11,8 +12,10 @@ public static class ResultsEndpoints
         app.MapGet("/api/results", ListResultsAsync);
         app.MapGet("/api/results/{id:guid}", GetResultAsync);
 
-        static async Task<IResult> ListResultsAsync(IMediator mediator)
-            => Results.Ok(await mediator.Send(new GetResultsQuery()));
+        static async Task<IResult> ListResultsAsync(IMediator mediator,
+            int pageNumber = 1,
+            int PageSize = 20)
+            => Results.Ok(await mediator.Send(new GetResults.Query(pageNumber, PageSize)));
 
         static async Task<IResult> GetResultAsync(Guid id, IMediator mediator, IScopedErrorService errorService)
             => await mediator.SendQueryAsync(new GetResultByIdQuery(id), errorService);
