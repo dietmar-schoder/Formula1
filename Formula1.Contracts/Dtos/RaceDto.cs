5094459dc24e485cf3d7468b33a3d7bebb6631c0
dietmar-schoder <dietmar@schoder.uk>
2024-11-22 16:10:25 +0000

    F1-50

--- Formula1.Contracts/Dtos/RaceDto.cs
+++ Formula1.Contracts/Dtos/RaceDto.cs
@@ -5,22 +5,27 @@ namespace Formula1.Contracts.Dtos;
 public record RaceDto(
     int Id,
     int SeasonYear,
+    string SeasonWikipediaUrl,
     int Round,
     string WikipediaUrl,
     int GrandPrixId,
     string GrandPrixName,
+    string GrandPrixWikipediaUrl,
     int CircuitId,
-    string CircuitName
-)
+    string CircuitName,
+    string CircuitWikipediaUrl)
 {
     public static RaceDto FromRace(Race race)
         => new(
             race.Id,
             race.SeasonYear,
+            race.Season.WikipediaUrl,
             race.Round,
             race.WikipediaUrl,
             race.GrandPrixId,
             race.GrandPrix.Name,
+            race.GrandPrix.WikipediaUrl,
             race.CircuitId ?? 0,
-            race.Circuit.Name);
+            race.Circuit.Name,
+            race.Circuit.WikipediaUrl);
 }
